d866746f52351d6517a93be0a6a9b1c2c7d4175d
Neel Maniar <136578611+NeelManiar@users.noreply.github.com>
2026-01-12 17:32:35 +0530

    Add files via upload

---
+++ audiobook.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+
+book = open("Poverty.pdf", "rb")
+pdfReader = PyPDF2 .PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+friend = pyttsx3.init()
+page = pdfReader.getPage(0 and 1)
+text = page.extractText()
+friend.say(text)
+friend.runAndWait()
